12a121da7941d3f99329a761094ff700d43f382e
Rafael <lucas.js@estudante.iftm.edu.br>
2024-09-05 21:17:52 -0300

    Projeto Final

---
+++ lead-manager/src/main/java/com/example/lead_manager/LeadManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lead_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadManagerApplication.class, args);
+	}
+
+}
---
+++ lead-manager/src/test/java/com/example/lead_manager/LeadManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lead_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
