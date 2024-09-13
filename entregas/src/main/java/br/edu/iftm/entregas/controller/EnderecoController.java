1bc6655fe0cbc371d151f77415feb5886709a5bf
Rafael <lucas.js@estudante.iftm.edu.br>
2024-09-13 15:39:24 -0300

    Remove unused import
    Import 'org.springframework.beans.factory.annotation.Autowired' is never used

--- entregas/src/main/java/br/edu/iftm/entregas/controller/EnderecoController.java
+++ entregas/src/main/java/br/edu/iftm/entregas/controller/EnderecoController.java
@@ -1,6 +1,5 @@
 package br.edu.iftm.entregas.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import br.edu.iftm.entregas.model.Endereco;
