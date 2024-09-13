0ce5a02856504df47d79598df1d56a344a301dc3
Rafael <lucas.js@estudante.iftm.edu.br>
2024-09-13 15:48:23 -0300

    Replace System.out by a logger.
    Logger permite controlar o nível de detalhes das mensagens (erro, aviso, informação), direcionar a saída para diferentes lugares (arquivo, console), e configurar ou desativar facilmente as mensagens em produção, enquanto System.out só imprime no console sem controle.

--- entregas/src/main/java/br/edu/iftm/entregas/EntregasApplication.java
+++ entregas/src/main/java/br/edu/iftm/entregas/EntregasApplication.java
@@ -1,38 +1,42 @@
-package br.edu.iftm.entregas;
+    package br.edu.iftm.entregas;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
+    import org.springframework.boot.CommandLineRunner;
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.edu.iftm.entregas.model.Pacote;
-import br.edu.iftm.entregas.model.Rastreamento;
-import br.edu.iftm.entregas.model.Endereco;
+    import br.edu.iftm.entregas.model.Pacote;
+    import br.edu.iftm.entregas.model.Rastreamento;
+    import br.edu.iftm.entregas.model.Endereco;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
+    import java.sql.Date;
+    import java.text.SimpleDateFormat;
 
-@SpringBootApplication
-public class EntregasApplication implements CommandLineRunner {
+    @SpringBootApplication
+    public class EntregasApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EntregasApplication.class, args);
+        private static final Logger logger = LoggerFactory.getLogger(EntregasApplication.class);
+
+        public static void main(String[] args) {
+            SpringApplication.run(EntregasApplication.class, args);
+        }
+
+        @Override
+        public void run(String... args) throws Exception {
+            
+            Endereco endereco = new Endereco(123, "Rua 1", "Bairro 1", "Cidade 1", "Minas Gerais", "12345-678");
+
+            Pacote pacote = new Pacote(1, "João", endereco, "Em trânsito");
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            java.util.Date parsedDate = dateFormat.parse("2021-10-10 10:00");
+            Date dataHora = new Date(parsedDate.getTime());
+
+            Rastreamento rastreamento = new Rastreamento(123, "202020", dataHora, "Em trânsito", "Rua 1", pacote);
+
+            logger.info("Pacotes: {}", pacote);
+            logger.info("Rastreamentos: {}", rastreamento); 
+            logger.info("Endereços: {}", endereco);
+        }
     }
-
-    @Override
-    public void run(String... args) throws Exception {
-        
-        Endereco endereco = new Endereco(123, "Rua 1", "Bairro 1", "Cidade 1", "Minas Gerais", "12345-678");
-
-        Pacote pacote = new Pacote(1, "João", endereco, "Em trânsito");
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        java.util.Date parsedDate = dateFormat.parse("2021-10-10 10:00");
-        Date dataHora = new Date(parsedDate.getTime());
-
-        Rastreamento rastreamento = new Rastreamento(123, "202020", dataHora, "Em trânsito", "Rua 1", pacote);
-
-        System.out.println("Pacotes: " + pacote);
-        System.out.println("Rastreamentos: " + rastreamento); 
-        System.out.println("Endereços: " + endereco);
-    }
-}
